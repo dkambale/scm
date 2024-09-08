df3d9629e7d20b4f1f71cec44237afb4c69421d9
dadakambale18@gmail.com <dadakambale18@gmail.com>
2024-09-08 22:50:46 +0530

    adding login method

--- src/main/java/com/scm/app/repo/StudentRepo.java
+++ src/main/java/com/scm/app/repo/StudentRepo.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.scm.app.model.Student;
 
-public interface StudentRepo extends JpaRepository<Student,Integer>{
+public interface StudentRepo extends JpaRepository<Student, Integer> {
+
+	Student getByUserNameAndPassword(String userName, String password);
 
 }
--- src/main/java/com/scm/app/repo/TeacherRepo.java
+++ src/main/java/com/scm/app/repo/TeacherRepo.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.scm.app.model.Teacher;
 
-public interface TeacherRepo extends JpaRepository<Teacher,Integer>{
+public interface TeacherRepo extends JpaRepository<Teacher,Long>{
+	
+	
+	Teacher getByUserNameAndPassword(String userName, String password);
 
 }
